38aaa88f3385b4a79daa06b8b810c0c423624951
Ruthvik Kamarasu <kamarasuruthvik@gmail.com>
2022-05-05 17:41:09 +0530

    Updatad code to send random article to telegram:

--- src/index.js
+++ src/index.js
@@ -9,20 +9,23 @@ const TOKEN = '5361534699:AAEFXUZKVpVJALCiIkYIJXfUgfis_eIZ2jw';
 const bot = new telegramBot(TOKEN, {polling: true});
 
 console.log('This is the token ',TOKEN);
-async function sendMessage(){
+async function getArticle(){
     try{
         const response = await axios.get("https://en.wikipedia.org/api/rest_v1/page/random/summary");
         
         console.log('The request is successful!!');
-        console.log(response);
+        return response.data;
     }
     catch(error){
         console.log(error);
     }
 }
-bot.on('message', (message) => {
+bot.on('message', async (message) => {
 
-    sendMessage();
+    let data= await getArticle();
+    const chatId= message.chat.id;
+    bot.sendMessage(chatId, data.title);
+    bot.sendMessage(chatId, data.extract);
 })
 
 
